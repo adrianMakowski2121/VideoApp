54c5e0a99c9be4999738c0567489a09fb003f473
Adrian <adrianmakowski2121@gmail.com>
2022-07-14 22:14:52 +0200

    dodanie metody, która wyświetla seriale i filmy jednocześnie

--- src/application/app/VideoControl.java
+++ src/application/app/VideoControl.java
@@ -11,12 +11,12 @@ public class VideoControl {
     private final static int ADD_SERIAL = 2;
     private final static int PRINT_SERIAL = 3;
     private final static int PRINT_FILM = 4;
+    private final static int PRINT_ALL_LISTS = 5;
 
     Reader reader = new Reader();
     Storage storage = new Storage();
 
     public void controlLoop() {
-
         int option;
 
         do {
@@ -38,12 +38,19 @@ public class VideoControl {
                 case PRINT_FILM:
                     storage.printFilms();
                     break;
+                case PRINT_ALL_LISTS:
+                    printAllLists();
+                    break;
                 default:
                     System.out.println("podales złą wartość");
                     break;
             }
         } while (option != EXIT);
+    }
 
+    public void printAllLists() {
+        storage.printSerials();
+        storage.printFilms();
     }
 
     private void addSerial() {
@@ -54,11 +61,12 @@ public class VideoControl {
     public void printOptions() {
         System.out.println(" ");
         System.out.println("wybierz jedną z opcji");
-        System.out.println(EXIT + " -wyjscie z programu");
-        System.out.println(ADD_VIDEO + " -dodaj film");
-        System.out.println(ADD_SERIAL + " -dodaj serial");
-        System.out.println(PRINT_SERIAL + " -wyswietl wszystkie seriale");
-        System.out.println(PRINT_FILM + " -wyswietl wszystkie filmy");
+        System.out.println(EXIT + " -> wyjscie z programu");
+        System.out.println(ADD_VIDEO + " -> dodaj film");
+        System.out.println(ADD_SERIAL + " -> dodaj serial");
+        System.out.println(PRINT_SERIAL + " -> wyswietl wszystkie seriale");
+        System.out.println(PRINT_FILM + " -> wyswietl wszystkie filmy");
+        System.out.println(PRINT_ALL_LISTS + " -> wyświetl seriale i filmy");
     }
 
     private void addVideo() {
