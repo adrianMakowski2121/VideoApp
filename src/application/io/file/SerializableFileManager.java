94bb8a3b9ab1b82dc89f7a3bb9c4c492e98c7dec
Adrian <adrianmakowski2121@gmail.com>
2022-07-23 13:28:41 +0200

    dodanie klasy serializujacej

--- src/application/io/file/SerializableFileManager.java
+++ src/application/io/file/SerializableFileManager.java
@@ -1,4 +1,45 @@
 package application.io.file;
 
-public class SerializableFileManager {
+import application.exceptions.DataExportException;
+import application.exceptions.DataImportException;
+import application.model.Storage;
+
+import java.io.*;
+
+public class SerializableFileManager implements FileManager {
+    private static final String FILE_NAME = "Library.txt";
+
+    @Override
+    public Storage importData() {
+
+        try (FileInputStream fis = new FileInputStream(FILE_NAME);
+             ObjectInputStream ois = new ObjectInputStream(fis);
+        ) {
+
+            return (Storage) ois.readObject();
+        } catch (FileNotFoundException e) {
+            throw new DataImportException("nie znaleziono pliku " + FILE_NAME);
+        } catch (IOException e) {
+            throw new DataImportException("błąd przy wczytywaniu pliku " + FILE_NAME);
+        } catch (ClassNotFoundException e) {
+            throw new DataImportException("Niezgodny typ danych w pliku " + FILE_NAME);
+        }
+
+    }
+
+    @Override
+    public void exportData(Storage storage) {
+        try (
+                FileOutputStream fos = new FileOutputStream(FILE_NAME);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+        ) {
+
+            oos.writeObject(storage);
+        } catch (FileNotFoundException e) {
+            throw new DataExportException("Brak pliku " + FILE_NAME);
+        } catch (IOException e) {
+            throw new DataExportException("Błąd zapisu danych do pliku " + FILE_NAME);
+        }
+
+    }
 }
