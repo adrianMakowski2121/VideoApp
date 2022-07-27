5e1a3c0b14141485f10dd50f5a13792704a6aff1
Adrian <adrianmakowski2121@gmail.com>
2022-07-27 19:11:29 +0200

    dodanie logiki do klas user i storageUser

--- src/application/model/StorageUser.java
+++ src/application/model/StorageUser.java
@@ -1,4 +1,53 @@
 package application.model;
 
-public class StorageUser {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class StorageUser extends User{
+    private List<Video> videoHistory = new ArrayList<>();
+    private List<Video> borrowedVideo = new ArrayList<>();
+
+
+    public StorageUser(String firsName, String lastName, int age) {
+        super(firsName, lastName, age);
+    }
+
+    public List<Video> getVideoHistory() {
+        return videoHistory;
+    }
+
+    public List<Video> getBorrowedVideo() {
+        return borrowedVideo;
+    }
+
+    private void addVideoToHistory(Video video) {
+        videoHistory.add(video);
+    }
+
+    public void borrowVideo(Video video) {
+        borrowedVideo.add(video);
+    }
+
+    private void returnVideo(Video video) {
+        boolean flag = false;
+        if (borrowedVideo.remove(video)) {
+            flag = true;
+            addVideoToHistory(video);
+        }
+     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        StorageUser that = (StorageUser) o;
+        return Objects.equals(videoHistory, that.videoHistory) && Objects.equals(borrowedVideo, that.borrowedVideo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), videoHistory, borrowedVideo);
+    }
 }
--- src/application/model/User.java
+++ src/application/model/User.java
@@ -1,4 +1,62 @@
 package application.model;
 
-public class User {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private String firsName;
+    private String lastName;
+    private int age;
+
+    public User(String firsName, String lastName, int age) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User = " +
+                "firsName='" + firsName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(firsName, user.firsName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firsName, lastName, age);
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
